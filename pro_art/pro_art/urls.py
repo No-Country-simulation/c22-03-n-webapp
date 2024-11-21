f0c20798ffcac6d62192bc34095e0c73d8ffe8c9
Marcos Mateo <106215917+KMBMarcos@users.noreply.github.com>
2024-11-20 22:41:12 -0400

    fix: correccion en el titulo de la pagina.

--- pro_art/pro_art/urls.py
+++ pro_art/pro_art/urls.py
@@ -24,9 +24,9 @@ from products.views import ProductDetail
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Pro Art API",
       default_version='v1',
-      description="Test description",
+      description="API documentation for the Pro Art project",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
